a73b8a1fc0d438e30f7d05375045e1b628b1efb6
Femi Ojo <nifemiojo13@yahoo.co.uk>
2022-05-17 00:02:21 +0100

    New model and basic info api

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using System.Collections;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Pokedex.Controllers
 {
@@ -7,9 +11,24 @@ namespace Pokedex.Controllers
     public class PokemonController : ControllerBase
     {
         [HttpGet( "{PokemonName}")]
-        public ActionResult Get ( string PokemonName )
+        public async Task<ActionResult<Pokemon>> Get ( string PokemonName )
         {
-            return Ok( );
+            var uri = $"https://pokeapi.co/api/v2/pokemon-species/{PokemonName}";
+
+            using var client = new HttpClient( );
+            var response = await client.GetStringAsync( uri );
+
+            JsonNode responseNode = JsonNode.Parse( response )!;
+            
+            Pokemon jsonString = new Pokemon
+            {
+                Name = responseNode? [ "name" ]?.ToString( ) ?? string.Empty,
+                Description = responseNode?[ "form_descriptions" ]?.AsArray().Count == 0 ? "" : responseNode? [ "form_descriptions" ]? [ 0 ]? [ "description" ]?.ToString( ) ?? string.Empty,
+                Habitat = responseNode?[ "habitat" ]? [ "name" ]?.ToString( ) ?? string.Empty,
+                isLegendary = ( bool ) responseNode? [ "is_legendary" ]!
+            }; 
+
+            return Ok( jsonString );
         }
     }
 }
---
+++ Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokedex.Models
+{
+	public class Pokemon
+	{
+		public string? Name { get; set; } = string.Empty;
+		public string? Description { get; set; } = string.Empty ;
+		public string? Habitat { get; set; } = string.Empty;
+		public bool isLegendary { get; set; }
+	}
+}
